7976add97b0a247b4f6569dfa8c7c6543f37250b
Pascal Nehlsen <pascaldev92@gmail.com>
2024-10-12 22:22:21 +0200

    Deploy website - based on 836cc9ca24bc147d2378c44b2795319274ffcc67

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-// src/pages/index.js
-import React from 'react';
-import { Redirect } from 'react-router-dom';
-
-const RedirectToIntro = () => {
-    return <Redirect to="/devsecops-blog/docs/projects/intro" />;
-};
-
-export default RedirectToIntro;
